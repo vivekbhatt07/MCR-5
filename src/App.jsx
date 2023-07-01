ad067d1559affae3f3cf69012f36002a27f651f7
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-07-01 16:36:36 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+
+const App = () => {
+  return <div className="text-4xl">Hello World</div>;
+};
+
+export default App;
