e468fe3e113666ade0cb6221ac1c1dd598edc478
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-07-01 22:46:23 +0530

    ADD IMG: Detail Page

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -16,7 +16,9 @@ const Detail = () => {
       <div className="max-w-[1280px] mx-auto flex flex-col gap-6">
         <h3 className="text-center">{getDish?.recipeName}</h3>
         <div className="flex gap-5 flex-col">
-          <div className="w-[300px] h-[200px] bg-[#ddd] border border-blue-950"></div>
+          <div className="w-[300px] h-[200px] bg-[#ddd] border border-blue-950">
+            <img src={image} alt="img" className="w-full h-full object-cover" />
+          </div>
           <div className="flex flex-col gap-4">
             <p className="flex justify-between gap-2">
               <strong>Cuisine Type</strong>
